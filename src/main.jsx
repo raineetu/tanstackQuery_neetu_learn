17826d2e402006318118092a0a113f9cdb095ece
Raineetu <raineetu0070@gmail.com>
2025-05-14 22:02:41 +0545

    Added devtools in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-//create a queryCLient instance
+// Create a QueryClient instance
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
