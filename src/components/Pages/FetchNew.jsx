58922b2e9792f2e3e70c0ac465700cf3f0633d1f
Raineetu <raineetu0070@gmail.com>
2025-05-14 19:47:41 +0545

    useQuery feature with dummy api added

--- src/components/Pages/FetchNew.jsx
+++ src/components/Pages/FetchNew.jsx
@@ -1,5 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchPosts = async () => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return response.data;
+};
+
 const FetchNew = () => {
-  return <div>Hello New Way</div>;
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["posts"], //Automatically cached by key...even if we use in another component then it fetch the cached data instead of erfetching
+    queryFn: fetchPosts,
+    refetchInterval: 1000, //refetch every 1 sec
+    refetchOnWindowFocus: true, //if user switches tabs and returns, React Query silently refetches data in the background
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  // console.log(data[0].body, "sfds");
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      {data.map((post) => (
+        <div key={post.id}>
+          <h4>{post.title}</h4>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default FetchNew;
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div>Hello Home</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/new">FetchNew</Link>
+        </li>
+        <li>
+          <Link to="/old">FetchOld</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
